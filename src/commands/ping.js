4902448675878e0ed8330aac2a85fa6665f0cdf3
RadarController <97064113+RadarController@users.noreply.github.com>
2026-04-11 15:31:38 +0100

    Add ping command

---
+++ src/commands/ping.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Check that Dispatch Bot is online.'),
+
+  async execute(interaction) {
+    await interaction.reply({
+      content: 'Dispatch Bot is online and responding.',
+      ephemeral: true
+    });
+  }
+};
