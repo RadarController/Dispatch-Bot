2a22a8a11642cf4dd60678198cb223182798f5a2
RadarController <97064113+RadarController@users.noreply.github.com>
2026-04-17 17:29:48 +0100

    Update command registration logic in ready.js
    Refactor command registration to support guild-specific commands.

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,15 +3,41 @@ const { ensureRolePanels } = require('../rolesPanel');
 const { REST, Routes } = require('discord.js');
 const { config } = require('../config');
 
-async function registerGlobalCommands(client) {
+// async function registerGlobalCommands(client) {
+//  const commands = Array.from(client.commands.values()).map((command) => command.data.toJSON());
+//  const rest = new REST({ version: '10' }).setToken(config.discordToken);
+//
+//  await rest.put(
+//    Routes.applicationCommands(config.discordClientId),
+//    { body: commands }
+//  );
+//}
+
+// this is temp
+
+async function registerCommands(client) {
   const commands = Array.from(client.commands.values()).map((command) => command.data.toJSON());
   const rest = new REST({ version: '10' }).setToken(config.discordToken);
+
+  const guildId = process.env.DISCORD_GUILD_ID;
+
+  if (guildId) {
+    await rest.put(
+      Routes.applicationGuildCommands(config.discordClientId, guildId),
+      { body: commands }
+    );
+    console.log(`Registered ${client.commands.size} guild command(s) to ${guildId}.`);
+    return;
+  }
 
   await rest.put(
     Routes.applicationCommands(config.discordClientId),
     { body: commands }
   );
+  console.log(`Registered ${client.commands.size} global command(s).`);
 }
+
+// bit above is temp
 
 module.exports = async (client) => {
   console.log(`Logged in as ${client.user.tag}`);
